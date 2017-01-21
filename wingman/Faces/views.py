c38f0ce4b956fdf1f4e417de5994363d5aef330e
eminkin97 <eminkin99@gmail.com>
2017-01-21 12:47:55 -0500

    changing around views and settings

--- wingman/Faces/views.py
+++ wingman/Faces/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Let's sort some Faces bro.")
+    return HttpResponse('yo')
