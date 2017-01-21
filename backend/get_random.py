6846fafb47d7a2373a47afbaf0ed0714e78511ce
DawnOfHammers <hairuo.sang@gmail.com>
2017-01-21 17:58:52 -0500

    idk again

---
+++ backend/get_random.py
@@ -0,0 +1,20 @@
+from os import listdir
+from os.path import isfile, join
+import random
+
+def get(gender):
+
+	if(gender == 'male'):
+		directory = 'dataset/male'
+		num_images = 10
+	if(gender == 'female'):
+		directory = 'dataset/female'
+		num_images = 10
+	if(gender == 'both'):
+		directory = 'dataset/both'
+		num_images = 10
+
+	files = [f for f in listdir(directory) if isfile(join(directory, f))]
+	random_sample = [ files[i] for i in sorted(random.sample(xrange(len(files)), num_images)) ]
+
+	return random_sample
