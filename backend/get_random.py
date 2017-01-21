4a1f324df4bc6b89c64e0ba82f9f7738858793cd
Avin Abraham <avin.abraham@rutgers.edu>
2017-01-21 16:42:52 -0500

    get_random

--- backend/get_random.py
+++ backend/get_random.py
@@ -6,13 +6,13 @@ def get(gender):
 
 	if(gender == 'male'):
 		directory = 'dataset/male'
-		num_images = 1
+		num_images = 10
 	if(gender == 'female'):
 		directory = 'dataset/female'
-		num_images = 1
+		num_images = 10
 	if(gender == 'both'):
 		directory = 'dataset/both'
-		num_images = 1
+		num_images = 10
 
 	files = [f for f in listdir(directory) if isfile(join(directory, f))]
 	random_sample = [ files[i] for i in sorted(random.sample(xrange(len(files)), num_images)) ]
