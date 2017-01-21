ddd2f76c8708c65255bc81dae09578d921db8dc0
Avin Abraham <avin.abraham@rutgers.edu>
2017-01-21 15:09:22 -0500

    backend

---
+++ backend/get_random.py
@@ -0,0 +1,24 @@
+from os import listdir
+from os.path import isfile, join
+import random
+
+def get(gender):
+
+	if(gender == 'male'):
+		directory = 'dataset/male'
+		num_images = 468
+	if(gender == 'female'):
+		directory = 'dataset/female'
+		num_images = 1063
+	if(gender == 'both'):
+
+		directory = 'dataset/both'
+		num_images = 1113
+
+	files = [f for f in listdir(directory) if isfile(join(directory, f))]
+	random_sample = [ files[i] for i in sorted(random.sample(xrange(len(files)), num_images)) ]
+
+	return random_sample
+
+
+	
